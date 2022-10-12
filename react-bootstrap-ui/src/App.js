31bb3aae8d406a60d9275d5553281529bef21067
mwz73trz <michaelzura@bellsouth.net>
2022-10-12 04:51:55 -0400

    complete

---
+++ react-bootstrap-ui/src/App.js
@@ -0,0 +1,18 @@
+import ButtonDemo from "./ButtonDemo";
+import AlertDemo from "./AlertDemo";
+import SpinnerDemo from "./SpinnerDemo";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>React Bootstrap UI</h1>
+        <ButtonDemo />
+        <AlertDemo />
+        <SpinnerDemo />
+      </header>
+    </div>
+  );
+}
+
+export default App;
